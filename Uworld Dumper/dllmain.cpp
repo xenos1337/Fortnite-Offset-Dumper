858749affc6ed9fd8c0076ed12e402b5b8e32170
xenos#1337 <66328734+xenos1337@users.noreply.github.com>
2021-04-22 16:01:37 +0200

    Add files via upload

---
+++ Uworld Dumper/dllmain.cpp
@@ -0,0 +1,61 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <Windows.h>
+#include <psapi.h>
+#include <intrin.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+PVOID* uWorld = 0;
+
+BOOLEAN MaskCompare(PVOID buffer, LPCSTR pattern, LPCSTR mask) {
+	for (auto b = reinterpret_cast<PBYTE>(buffer); *mask; ++pattern, ++mask, ++b) {
+		if (*mask == 'x' && *reinterpret_cast<LPCBYTE>(pattern) != *b) {
+			return FALSE;
+		}
+	}
+
+	return TRUE;
+}
+
+PBYTE FindPattern(LPCSTR pattern, LPCSTR mask) {
+	MODULEINFO info = { 0 };
+	GetModuleInformation(GetCurrentProcess(), GetModuleHandle(0), &info, sizeof(info));
+
+	info.SizeOfImage -= static_cast<DWORD>(strlen(mask));
+
+	for (auto i = 0UL; i < info.SizeOfImage; ++i) {
+		auto addr = reinterpret_cast<PBYTE>(info.lpBaseOfDll) + i;
+		if (MaskCompare(addr, pattern, mask)) {
+			return addr;
+		}
+	}
+}
+
+BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID reserved) {
+	if (reason == DLL_PROCESS_ATTACH) {
+		try 
+		{
+			AllocConsole();
+			static_cast<VOID>(freopen("CONIN$", "r", stdin));
+			static_cast<VOID>(freopen("CONOUT$", "w", stdout));
+			static_cast<VOID>(freopen("CONOUT$", "w", stderr));
+
+			std::cout << "Uworld Dumper by xenos#1337" << "\n" << std::endl;
+			std::cout << "-------------------" << "\n" << std::endl;
+			std::cout << "uWorld: " << "0x" << std::hex << (uintptr_t)reinterpret_cast<decltype(uWorld)>((PBYTE)((UINT_PTR)(FindPattern("\x48\x8B\x05\x00\x00\x00\x00\x4D\x8B\xC2", "xxx????xxx")) + *(PINT)((UINT_PTR)(FindPattern("\x48\x8B\x05\x00\x00\x00\x00\x4D\x8B\xC2", "xxx????xxx"))+((7)-sizeof(INT))) + (7))) - (uintptr_t)GetModuleHandle(L"FortniteClient-Win64-Shipping.exe") << "\n" << std::endl;
+			std::cout << "--------------------" << "\n" << std::endl;
+			std::cout << "Done :)" << "\n" << std::endl;
+		}
+		catch(exception e)
+		{
+			std::cout << e.what() << "\n" << std::endl;
+		}
+	}
+
+	return TRUE;
+}
